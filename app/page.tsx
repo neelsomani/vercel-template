e968b35b3acb28c40e2477e3fdd78c7360fb3d90
Neel Somani <neeljaysomani@gmail.com>
2025-04-24 23:54:33 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-r from-slate-50 to-slate-100">
+      <h1 className="text-4xl font-bold text-slate-800 sm:text-6xl md:text-7xl">Hello World</h1>
+    </main>
+  )
+}
